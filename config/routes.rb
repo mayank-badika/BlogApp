ba7a4a029a212a9e19345a0db28a02de196b04b0
mayank-badika <mayank.badika@snapdocs.com>
2021-11-29 12:38:47 -0700

    Used partials for duplicate code Added ability to comment and delete comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "articles#index" # rooting the first page to
-  resources :articles
+  resources :articles do # creating heirarchy with articles to generate routes accordingly
+  resources :comments
+  end
 end
